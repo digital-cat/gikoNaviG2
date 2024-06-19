3751f76a64a2ca037152f11ff1106dd078ffabfc
digital-cat <super-dacho@mbp.nifty.com>
2024-06-19 22:45:29 +0900

    ギコナビリリース b75 898

--- GikoSystem.pas
+++ GikoSystem.pas
@@ -341,7 +341,7 @@ const
 		  'ProductVersion',
 		  'SpecialBuild');
 
-	UAVers: array[0..16] of TUAVer = (
+	UAVers: array[0..17] of TUAVer = (
       (BetaVer:  0; FileVer: ''),
       (BetaVer: 74; FileVer: '1.75.0.881'),
       (BetaVer: 74; FileVer: '1.75.0.883'),
@@ -358,7 +358,8 @@ const
       (BetaVer: 74; FileVer: '1.75.0.894'),
       (BetaVer: 74; FileVer: '1.75.0.895'),
       (BetaVer: 74; FileVer: '1.75.0.896'),
-      (BetaVer: 74; FileVer: '1.75.0.897')
+      (BetaVer: 74; FileVer: '1.75.0.897'),
+      (BetaVer: 74; FileVer: '1.75.0.898')
 	);	// 当面リリースの度にバージョン情報を追加していく
 
 // *************************************************************************
