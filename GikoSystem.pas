46aa5922ed4f572a952046dc9fa981cf68dc900c
digital-cat <super-dacho@mbp.nifty.com>
2024-07-14 19:56:13 +0900

    ギコナビリリース b75 902

--- GikoSystem.pas
+++ GikoSystem.pas
@@ -341,7 +341,7 @@ const
 		  'ProductVersion',
 		  'SpecialBuild');
 
-	UAVers: array[0..20] of TUAVer = (
+	UAVers: array[0..21] of TUAVer = (
       (BetaVer:  0; FileVer: ''),
       (BetaVer: 74; FileVer: '1.75.0.881'),
       (BetaVer: 74; FileVer: '1.75.0.883'),
@@ -362,7 +362,8 @@ const
       (BetaVer: 74; FileVer: '1.75.0.898'),
       (BetaVer: 74; FileVer: '1.75.0.899'),
       (BetaVer: 74; FileVer: '1.75.0.900'),
-      (BetaVer: 74; FileVer: '1.75.0.901')
+      (BetaVer: 74; FileVer: '1.75.0.901'),
+      (BetaVer: 74; FileVer: '1.75.0.902')
 	);	// 当面リリースの度にバージョン情報を追加していく
 
 // *************************************************************************
--- readme/gikoSetup.iss
+++ readme/gikoSetup.iss
@@ -11,12 +11,12 @@ DefaultDirName=C:\gikonavi
 VersionInfoDescription=ギコナビ(避難所版II)セットアッププログラム
 DefaultGroupName=ギコナビ
 ; 自分の環境に合わせてください
-SourceDir=D:\giko\Setup\Release75-900
+SourceDir=D:\giko\Setup\Release75-902
 OutputDir=D:\giko\Setup\Output
 SetupIconFile=D:\giko\Setup\GikoSetup.ico
 ; ギコナビのバージョンにあわせてください
-VersionInfoVersion=1.75.0.900
-OutputBaseFilename=gikoNavi_b75_900_setup
+VersionInfoVersion=1.75.0.902
+OutputBaseFilename=gikoNavi_b75_902_setup
 
 
 
